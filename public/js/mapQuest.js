fd8d525ec1a0cc5a69aa43de9437778b483db01b
igneel73 <igneel.kush73@gmail.com>
2019-02-28 13:07:43 -0800

    meh

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'C9c291hwYaGhEAHAdRDXmSVApjXMK9Uu';
+
+	var map = L.mapquest.map('map', {
+		center: [32.88, -117.236],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 14,
+		zoomControl:  false
+	});
+
+	L.marker([32.88, -117.236]).addTo(map);
 }
